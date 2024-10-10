75b3d6a2245bbe854f6d119ba76c39b794ff809c
ShaneHtetAung <shanehtetaung2842004@gmail.com>
2024-10-10 12:17:41 +0630

    Node Startup

---
+++ routes/post.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+router.route("/")
+    .get((req, res) => res.json({ msg: "All posts" }))
+    .post((req, res) => res.json(req.body))
+
+
+router.route("/:id")
+    .get((req, res) => res.json({ msg: "The request id is " + req.params.id }))
+    .patch((req, res) => res.json({ msg: "Edit id is " + req.params.id }))
+    .delete((req, res) => res.json({ msg: "Delete is is " + req.params.id }))
+
+// router.get("/:id", (req, res) => {
+//     let id = req.params.id;
+//     res.json({ msg: "The request id is " + id })
+// })
+
+// router.patch("/:id", (req, res) => {
+//     let id = req.params.id;
+//     res.json({
+//         msg: "Edit id is " + id,
+//     })
+// })
+
+// router.delete("/:id", (req, res) => {
+//     let id = req.params.id;
+//     res.json({
+//         msg: "Delete is is " + id
+//     })
+// })
+
+
+
+module.exports = router;
